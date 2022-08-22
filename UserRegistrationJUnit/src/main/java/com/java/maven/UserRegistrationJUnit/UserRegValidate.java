bb486bc37be293d3121806becb8ad36cf788c32c
MohanRaj <mraj86698@gmail.com>
2022-08-22 14:43:37 +0530

    Added to Welcome Messages

---
+++ UserRegistrationJUnit/src/main/java/com/java/maven/UserRegistrationJUnit/UserRegValidate.java
@@ -0,0 +1,10 @@
+package com.java.maven.UserRegistrationJUnit;
+
+
+public class UserRegValidate
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to JUnit to validate the User" );
+    }
+}
---
+++ UserRegistrationJUnit/src/test/java/com/java/maven/UserRegistrationJUnit/UserRegValidateTest.java
@@ -0,0 +1,20 @@
+package com.java.maven.UserRegistrationJUnit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class UserRegValidateTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
